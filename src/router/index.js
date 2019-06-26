7de62e164587fc6e5935aa162d6898c3a9ae894c
unknown <1109401935@qq.com>
2019-06-26 17:03:04 +0800

    supermembership

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Super from '../page/Super Membership .vue'
+import rem from "../rem"
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Super',
+      component: Super
     }
   ]
 })
