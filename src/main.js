7deab70ade16f57f79a7c00b88be2edd71eb9b21
cyc123321 <1617415468@qq.com>
2019-07-03 12:01:16 +0800

    erer

--- src/main.js
+++ src/main.js
@@ -3,27 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
-import css from "./router/common.css"
-import rem from './router/rem'
-import indexCss from "./page/index.css"
+import  "./router/common.css"
+import  './router/rem'
+import  "./page/index.css"
 import 'swiper/dist/css/swiper.css';
 import VueAwesomeSwiper from "vue-awesome-swiper"
 Vue.use(VueAwesomeSwiper)
 
 
 
-=======
->>>>>>> d2b1d5ea92974ac2e87c51d17c827a126d05c44e
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  css,
-  rem,
-  indexCss,
   components: { App },
   template: '<App/>'
 })
